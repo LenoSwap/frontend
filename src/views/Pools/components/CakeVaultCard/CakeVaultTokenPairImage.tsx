946406e99a5c885fb7dd75f30b3342912a1ae0db
Marquis Ogre <marquisogre@gmail.com>
2022-05-20 10:36:55 +0530

    Fix: Change token.address to token.symbol

--- src/views/Pools/components/CakeVaultCard/CakeVaultTokenPairImage.tsx
+++ src/views/Pools/components/CakeVaultCard/CakeVaultTokenPairImage.tsx
@@ -3,7 +3,7 @@ import { TokenPairImage, ImageProps } from '@quickswap-core/uikit'
 import { mainnetTokens } from 'config/constants/tokens'
 
 const CakeVaultTokenPairImage: React.FC<Omit<ImageProps, 'src'>> = (props) => {
-  const primaryTokenSrc = `/images/tokens/${mainnetTokens.quick.address}.svg`
+  const primaryTokenSrc = `/images/tokens/${mainnetTokens.quick.symbol}.png`
 
   return <TokenPairImage primarySrc={primaryTokenSrc} secondarySrc="/images/tokens/autorenew.svg" {...props} />
 }
